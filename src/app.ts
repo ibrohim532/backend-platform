8d1ce724a8b171b7f3604c865ce082c59c9d9ca8
ibrohim277 <farhodsadullayev2@gmail.com>
2026-07-08 13:31:19 +0500

    fix swagger server url and cors

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,9 @@ const app = express();
 
 // ==================== GLOBAL MIDDLEWARE ====================
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL || "*", credentials: true }));
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
